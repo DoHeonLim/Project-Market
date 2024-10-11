b533c431d6c3b75805ca117c2cd2d5659d9be059
[AI 10]임도헌 <ldh2233@gmail.com>
2024-10-11 16:56:43 +0900

    ✨ Feat : sms 로그인 기능 추가

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -10,13 +10,35 @@ Date        Author   Status    Description
 */
 "use server";
 
+import twilio from "twilio";
+import crypto from "crypto";
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
 
 interface IActionState {
   token: boolean;
 }
+
+const handleGetToken = async () => {
+  const token = crypto.randomInt(100000, 999999).toString();
+  // 여러 유저가 sms 사용 시 토큰값이 겹치면 다시 발급해야됨.
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return handleGetToken();
+  } else {
+    return token;
+  }
+};
 
 const phoneSchema = z
   .string()
@@ -25,10 +47,24 @@ const phoneSchema = z
     (phone) => validator.isMobilePhone(phone, "ko-KR"),
     "잘못된 번호 형식입니다."
   );
+
+const handleTokenExists = async (token: number) => {
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token: token.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(exists);
+};
+
 const tokenSchema = z.coerce
   .number()
   .min(100000, "인증번호는 6자리 입니다.")
-  .max(999999, "인증번호는 6자리 입니다.");
+  .max(999999, "인증번호는 6자리 입니다.")
+  .refine(handleTokenExists, "인증번호를 다시 입력해주세요.");
 
 export const smsLogin = async (prevState: IActionState, formData: FormData) => {
   const phone = formData.get("phone");
@@ -41,19 +77,72 @@ export const smsLogin = async (prevState: IActionState, formData: FormData) => {
         error: result.error.flatten(),
       };
     } else {
+      // 유저 SMS 토큰 삭제
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+      const token = await handleGetToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+      const client = twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN
+      );
+      await client.messages.create({
+        body: `당신의 Bubble-Market 인증 번호는 ${token}입니다.`,
+        from: process.env.TWILIO_PHONE_NUMBER!,
+        to: process.env.MY_PHONE_NUMBER!,
+      });
       return {
         token: true,
       };
     }
   } else {
-    const result = tokenSchema.safeParse(token);
+    const result = await tokenSchema.safeParseAsync(token);
     if (!result.success) {
       return {
         token: true,
         error: result.error.flatten(),
       };
     } else {
-      redirect("/");
+      const token = await db.sMSToken.findUnique({
+        where: {
+          token: result.data.toString(),
+        },
+        select: {
+          id: true,
+          userId: true,
+        },
+      });
+
+      const session = await getSession();
+      session.id = token!.userId;
+      await session.save();
+      await db.sMSToken.delete({
+        where: {
+          id: token!.id,
+        },
+      });
+
+      redirect("/profile");
     }
   }
 };

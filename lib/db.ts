da6fa9c16b6d8d39705177580ccb0718bda73f79
DoHeonLim <ldh2233@gmail.com>
2024-10-14 19:25:52 +0900

    📚 Docs : db history 주석 추가

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,13 @@
+/**
+File Name : lib/db
+Description : 프리즈마 클라이언트
+Author : 임도헌
+
+History
+Date        Author   Status    Description
+2024.10.06  임도헌   Created
+2024.10.06  임도헌   Modified  프리즈마 클라이언트 생성
+*/
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();

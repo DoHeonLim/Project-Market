94ec10094f97447a40b65d75e13c687800b16a65
DoHeonLim <ldh2233@gmail.com>
2024-12-10 23:53:16 +0900

    ✨ Feat : 게시판 모델에 게시판 이미지 모델 추가(이미지 여러개 하기 위해서)

--- app/posts/add/schema.ts
+++ app/posts/add/schema.ts
@@ -7,7 +7,7 @@
  Date        Author   Status    Description
  2024.11.23  임도헌   Created
  2024.11.23  임도헌   Modified  게시글 스키마 추가
- 
+ 2024.12.10  임도헌   Modified  게시글 photos 필드 추가(optional) 
 */
 
 import { z } from "zod";
@@ -21,6 +21,7 @@ export const postSchema = z.object({
   description: z.string({
     required_error: "내용을 적어주세요.",
   }),
+  photos: z.array(z.string()).optional(),
 });
 
 export type PostType = z.infer<typeof postSchema>;

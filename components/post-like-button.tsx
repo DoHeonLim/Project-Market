9544077d1ab0204da80765ff0b2c842ac8c2b7e3
DoHeonLim <ldh2233@gmail.com>
2025-05-26 17:47:14 +0900

    🔨 Refactor : 게시글 좋아요 기능 최적화

--- components/post-like-button.tsx
+++ components/post-like-button.tsx
@@ -10,13 +10,15 @@ Date        Author   Status    Description
 2024.11.06  임도헌   Modified  useOptimistic에 payload 사용하지 않아서 삭제
 2024.12.18  임도헌   Modified  sm:hidden 추가(모바일 반응형 추가)
 2024.12.24  임도헌   Modified  좋아요 버튼 아이콘 변경
+2025.05.10  임도헌   Modified  startTransition을 사용한 성능 최적화
 */
 "use client";
 
 import { HeartIcon } from "@heroicons/react/24/solid";
 import { HeartIcon as OutlineHeartIcon } from "@heroicons/react/24/outline";
-import { useOptimistic } from "react";
+import { useOptimistic, startTransition } from "react";
 import { dislikePost, likePost } from "@/app/posts/[id]/actions";
+import { toast } from "sonner";
 
 interface ILikeButtonProps {
   isLiked: boolean;
@@ -38,18 +40,29 @@ export default function PostLikeButton({
         : previousState.likeCount + 1,
     })
   );
+
   const handleClick = async () => {
-    reducerFn(undefined);
-    if (isLiked) {
-      await dislikePost(postId);
-    } else {
-      await likePost(postId);
+    startTransition(() => {
+      reducerFn(undefined);
+    });
+
+    try {
+      if (isLiked) {
+        await dislikePost(postId);
+        toast.success("좋아요를 취소했습니다.");
+      } else {
+        await likePost(postId);
+        toast.success("좋아요를 눌렀습니다.");
+      }
+    } catch {
+      toast.error("오류가 발생했습니다. 다시 시도해주세요.");
     }
   };
+
   return (
     <button
       onClick={handleClick}
-      className={`flex items-center gap-1 p-2 transition-colors
+      className={`flex items-center gap-1 transition-colors
         ${
           state.isLiked
             ? "text-rose-500"
